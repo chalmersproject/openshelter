8c7adf6eb345fca4630271e79a30c5d6332ca2dd
Mister Chalmers <info@chalmers.cards>
2023-06-11 17:00:00 -0400

    changes type UUID to type ID in shelter_signal_type graphql

--- app/graphql/types/shelter_signal_type.rb
+++ app/graphql/types/shelter_signal_type.rb
@@ -4,7 +4,7 @@ module Types
   class ShelterSignalType < Types::BaseObject
     field :id, ID, null: false
     field :type, String, null: false
-    field :shelter_id, Types::UuidType, null: false
+    field :shelter_id, ID, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
